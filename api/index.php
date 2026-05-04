3a531e7cf6a236e48ddab5f93c56c78e6f24523d
arigin <email-github-ariginanjar75@gmail.com>
2026-05-04 18:08:38 +0800

    Make api/index.php more robust for Vercel

--- api/index.php
+++ api/index.php
@@ -1,4 +1,23 @@
 <?php
 
-// Jalankan index.php utama Laravel
-require __DIR__ . '/../public/index.php';
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Tentukan root path
+$rootPath = __DIR__ . '/..';
+
+// Autoload
+require $rootPath . '/vendor/autoload.php';
+
+// Bootstrap
+$app = require_once $rootPath . '/bootstrap/app.php';
+
+// Jalankan aplikasi
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
